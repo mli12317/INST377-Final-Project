3a3230633178efd11de4ab60551e01007ca890a0
Sara <ssusa@terpmail.umd.edu>
2026-05-18 01:13:08 -0400

    nnnn

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/home_page.html');
 
 });
 
