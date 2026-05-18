f0c2e4c20c3dde6d56a0a305f185c87e5bdf298f
Sara <ssusa@terpmail.umd.edu>
2026-05-18 02:35:44 -0400

    nn

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static('public'));
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/artworks', artworksRoutes);
 
